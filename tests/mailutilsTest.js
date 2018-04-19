d850ffd44a97c560bf35db8b62d2dc96d867420c
Ruel Loehr <ruel.loehr@spanning.com>
2018-04-19 13:47:04 -0500

    iso charset support

--- tests/mailutilsTest.js
+++ tests/mailutilsTest.js
@@ -1,5 +1,7 @@
-var assert = require('assert');
-var mailutils = require('../lib/mailutils.js');
+const mock = require('mock-fs');
+const fs = require('fs');
+const assert = require('assert');
+const mailutils = require('../lib/mailutils.js');
 
 describe('Comment stripping', function() {
     describe('comments should be stripped when not in enclosed quotes', function() {
@@ -21,6 +23,25 @@ describe('Comment stripping', function() {
 });
 
 
+describe('Processing headers', function() {
+
+    mock({
+        'output.txt': ''
+    });
+
+    var fd = fs.openSync('output.txt', 'w');
+
+    describe('To headers should be detected and written to a file', function() {
+        it('Should handle folded headers', function() {
+            var results = mailutils.processDataLine('To: Ruel Loehr <ruelloehr@gmail.com>', '', fd);
+            var secondLine = mailutils.processDataLine('  has a folded header', results, fd);
+            assert.equal(secondLine, 'To: Ruel Loehr <ruelloehr@gmail.com>  has a folded header');
+        });
+    });
+
+});
+
+
 describe('RFC2074 encoding', function() {
     describe('Encoded headers should be decoded', function() {
         it('Should correctly decode header contents', function(){
@@ -29,18 +50,38 @@ describe('RFC2074 encoding', function() {
         });
     });
 
-    describe('Encoded headers should be decoded for paritally encoded items', function() {
+    describe('Encoded headers should be decoded for partially encoded items', function() {
         it('Should correctly decode header contents', function(){
             assert.equal(mailutils.decodeRFC2407('From: =?utf-8?q?Aviva?= <aviva@avivaemail.co.uk>'),  'From: Aviva <aviva@avivaemail.co.uk>');
         });
     });
 
-    describe('Base64 encoded headers should work', function() {
+    describe('Base64 encoded headers be correctly decoded', function() {
         it('Should correctly decode base64 header contents', function(){
             assert.equal(mailutils.decodeRFC2407('Subject: =?UTF-8?B?4pyIIEJvc3RvbiBhaXJmYXJlIGRlYWxzIC0gd2hpbGUgdGhleSBsYXN0IQ==?='),  'Subject: ✈ Boston airfare deals - while they last!');
         });
     });
 
+    describe('ASCII encoded headers be correctly decoded', function() {
+        it('Should correctly decode ascii header contents', function(){
+            assert.equal(mailutils.decodeRFC2407('From: =?US-ASCII?Q?Keith_Moore?= <moore@cs.utk.edu>'),  'From: Keith Moore <moore@cs.utk.edu>');
+        });
+    });
+
+    describe('ISO-8859-1 encoded headers be correctly decoded', function() {
+        it('Should correctly decode iso-8859-1 header contents', function(){
+            assert.equal(mailutils.decodeRFC2407('To: =?ISO-8859-1?Q?Keld_J=F8rn_Simonsen?= <keld@dkuug.dk>'),  'To: Keld Jørn Simonsen <keld@dkuug.dk>');
+        });
+
+        it('Should correctly decode iso-8859-1 header contents case 2', function(){
+            assert.equal(mailutils.decodeRFC2407('Subject: =?iso-8859-1?q?p=F6stal?='),  'Subject: pöstal');
+        });
+
+
+        it('Should correctly decode iso-8859-1  base 64 contents', function(){
+            assert.equal(mailutils.decodeRFC2407('To: =?ISO-8859-1?B?RnLmZGVyaWsgSPhsbGplbg==?= <fh@ez.no>'),  'To: Fræderik Hølljen <fh@ez.no>');
+        });
+    });
 
 
 
